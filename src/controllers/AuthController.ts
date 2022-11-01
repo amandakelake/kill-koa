a44f9b650c6b7f0e73edc5e328580d781a7dbad5
amandakelake <lguangcong@163.com>
2022-11-01 16:46:00 +0800

    feat: del default value

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,44 +1,44 @@
-import { Context } from 'koa';
 import * as argon2 from 'argon2';
-import { getManager } from 'typeorm';
-import { User } from '../entity/user';
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '../constants/jwt';
+import { Context } from 'koa';
+import { getManager } from 'typeorm';
 import { UnauthorizedException } from '../common/exceptions';
+import { JWT_SECRET } from '../constants/jwt';
+import { User } from '../entity/user';
 
 export default class AuthController {
-	public static async login(ctx: Context) {
-		const userRepository = getManager().getRepository(User);
+    public static async login(ctx: Context) {
+        const userRepository = getManager().getRepository(User);
 
-		const reqBody = ctx.request.body as Record<string, any>;
+        const reqBody = ctx.request.body as Record<string, any>;
 
-		const user = await userRepository
-		  .createQueryBuilder()
-		  .where({ name: reqBody.name })
-		  .addSelect('User.password')
-		  .getOne();
+        const user = await userRepository
+            .createQueryBuilder()
+            .where({ name: reqBody.name })
+            .addSelect('User.password')
+            .getOne();
 
-		if (!user) {
-			throw new UnauthorizedException('用户名不存在')
-		} else if (await argon2.verify(user.password, reqBody.password)) {
-			ctx.status = 200;
-			ctx.body = { token: jwt.sign({ id: user.id }, JWT_SECRET), id: user.id };
-		} else {
-			throw new UnauthorizedException('密码错误')
-		}
-	}
+        if (!user) {
+            throw new UnauthorizedException('用户名不存在');
+        } else if (await argon2.verify(user.password, reqBody.password)) {
+            ctx.status = 200;
+            ctx.body = { token: jwt.sign({ id: user.id }, JWT_SECRET), id: user.id };
+        } else {
+            throw new UnauthorizedException('密码错误');
+        }
+    }
 
-	public static async register(ctx: Context) {
-		const userRepository = getManager().getRepository(User);
-		const newUser = new User();
-		const { name = '', email = '', password = '' } = ctx.request.body as Record<string, any>;
-		newUser.name = name;
-		newUser.email = email;
-		newUser.password = await argon2.hash(password);
+    public static async register(ctx: Context) {
+        const userRepository = getManager().getRepository(User);
+        const newUser = new User();
+        const { name, email, password } = ctx.request.body as Record<string, any>;
+        newUser.name = name || '';
+        newUser.email = email || '';
+        newUser.password = await argon2.hash(password);
 
-		const user = await userRepository.save(newUser);
+        const user = await userRepository.save(newUser);
 
-		ctx.status = 201;
-		ctx.body = user;
-	}
+        ctx.status = 201;
+        ctx.body = user;
+    }
 }
